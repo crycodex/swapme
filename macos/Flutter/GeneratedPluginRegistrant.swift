89b8a31a774285532444a529209077d3236cc587
Cristhian David Recalde <crecalde@neaternotes.com>
2025-08-15 12:09:47 -0500

    Agrega dependencias de `google_mobile_ads` y `webview_flutter` en `pubspec.yaml`, actualiza `pubspec.lock` y `Podfile.lock` para incluir las versiones correspondientes. Se registra el plugin `webview_flutter_wkwebview` en `GeneratedPluginRegistrant.swift`, mejorando la funcionalidad de anuncios y visualización web en la aplicación.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,6 +17,7 @@ import flutter_local_notifications
 import google_sign_in_ios
 import sign_in_with_apple
 import video_player_avfoundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppBadgePlusPlugin.register(with: registry.registrar(forPlugin: "AppBadgePlusPlugin"))
@@ -31,4 +32,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
