8533e0d09fb1b260252c8cbe98e173db0f120f79
Cristhian David Recalde <crecalde@neaternotes.com>
2025-08-15 11:22:47 -0500

    Agrega botones de inicio de sesión social con iconos de Google y Apple en `social_login_buttons.dart`. Se actualiza `pubspec.yaml` para incluir las imágenes correspondientes y se registran nuevos plugins en `GeneratedPluginRegistrant.swift` y `generated_plugins.cmake` para notificaciones locales. Esto mejora la interfaz de usuario y la funcionalidad de inicio de sesión en la aplicación.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,23 +5,29 @@
 import FlutterMacOS
 import Foundation
 
+import app_badge_plus
 import cloud_firestore
 import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_database
+import firebase_messaging
 import firebase_storage
+import flutter_local_notifications
 import google_sign_in_ios
 import sign_in_with_apple
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppBadgePlusPlugin.register(with: registry.registrar(forPlugin: "AppBadgePlusPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
